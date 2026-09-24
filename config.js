8123ac2ba38462f1414864b15d08bdeb05c422d9
fletma <mafletcher@gmail.com>
2026-09-24 14:37:56 +1000

    Update config.js
    Change name to Mark.
    Update Weather defaultLatitude, defaultLongitude
    Add weatherKey

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'Sam',
+	name: 'Mark',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: false,
@@ -26,14 +26,14 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '004ac81db7532697ed9d2f5b55c02322', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '-33.826',
+	defaultLongitude: '151.201',
 
 	// Autochange
 	autoChangeTheme: true,
